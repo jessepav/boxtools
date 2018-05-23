5aa33f0988b9ccadc969c24cec657557bb7e9960
Jesse Pavel <jpavel@alum.mit.edu>
2018-05-22 21:17:17 -0400

    Add help to various command-line failures

--- src/com/elektrika/boxtools/BoxTools.java
+++ src/com/elektrika/boxtools/BoxTools.java
@@ -17,29 +17,32 @@ import java.nio.file.Paths;
 public class BoxTools
 {
     public static void main(String[] args) throws IOException, JsonException {
-        if (args.length == 0) {
-            System.out.println(
-                "Usage: BoxTools <command> [args]\n\n" +
-                "Commands:\n" +
-                "   -extract <filename.boxnote> <filename.txt>"
-            );
-            System.exit(1);
-        }
+        if (args.length == 0)
+            showHelpAndExit();
+
         switch (args[0]) {
         case "-extract":
             extractBoxNoteText(args, args.length - 1, 1);
             break;
         default:
-            System.exit(1);
-            break;
+            showHelpAndExit();
         }
         System.exit(0);
+    }
+
+    private static void showHelpAndExit() {
+        System.out.println(
+            "Usage: BoxTools <command> [args]\n\n" +
+            "Commands:\n" +
+            "   -extract <filename.boxnote> <filename.txt>"
+        );
+        System.exit(1);
     }
 
     private static void extractBoxNoteText(String[] args, int numArgs, int argsStart)
                 throws JsonException, IOException {
         if (numArgs != 2)
-            return;
+            showHelpAndExit();
 
         Path inPath = Paths.get(args[argsStart++]);
         Path outPath = Paths.get(args[argsStart++]);
