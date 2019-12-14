210c0745a790539b9136cdcf6c20eb4f3b96ae5e
Jesse Pavel <jpavel@alum.mit.edu>
2019-12-14 14:33:57 -0500

    Update package-source script

--- scripts/package-source.bat
+++ scripts/package-source.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0..
 del boxtools-src.7z
-"c:\Program Files\7-Zip\7z.exe" a boxtools-src.7z @scripts\srclist.txt -bb -xr!.*.marks -xr!.svn
+"c:\Program Files\7-Zip\7z.exe" a boxtools-src.7z @scripts\srclist.txt -bb -xr!.*.marks -xr!.svn -xr!*.jar
 popd
