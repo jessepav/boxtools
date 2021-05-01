ac7e744f931b5c6a5025441b5847da36391cc49a
Jesse Pavel <jpavel@alum.mit.edu>
2021-05-01 13:29:38 -0400

    Tweak backup-source script output

--- scripts/backup-source.bat
+++ scripts/backup-source.bat
@@ -1,5 +1,7 @@
 @echo off
 pushd %~dp0..
-call scripts\package-source.bat
+echo Packaging source...
+call scripts\package-source.bat 1> NUL 2>&1
+echo Uploading source...
 call boxtools.cmd -put version 576502560816 boxtools-src.7z
 popd
