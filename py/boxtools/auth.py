f5c0ac20eb4d1db85dac25679e010287afa453b5
Jesse Pavel <jpavel@alum.mit.edu>
2023-03-23 00:30:23 -0400

    Auth is working!

--- py/boxtools/auth.py
+++ py/boxtools/auth.py
@@ -2,41 +2,61 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 from threading import Thread
 
-port = 18444
+from boxsdk import OAuth2, Client
 
 http_server = None
+auth_url, csrf_token = None, None
+authcode = None
 
 class OAuthRequestHandler(BaseHTTPRequestHandler):
     server_version = "boxtools/0.1"
 
     def do_GET(self):
-        print("Path:", self.path)
+        global authcode
         url = urlparse(self.path)
         query_params = parse_qs(url.query)
-        print("Query Params:", query_params)
+        authcode = query_params.get('code', [None])[0]
+        ctoken = query_params.get('state', [None])[0]
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
         self.send_header('Cache-Control', 'no-store')
         self.end_headers()
-        if url.path == '/shutdown':
+        if authcode and ctoken == csrf_token:
+            self.wfile.write("Authcode retrieved -- return to boxtools".encode())
             shutdown_server()
-            self.wfile.write("Shutting down...".encode())
         else:
-            self.wfile.write("That URL, I'm afraid, has no meaning to me.".encode())
+            self.wfile.write("The request did not include appropriate 'code' and 'state' query parameters".encode())
+            print(authcode, ctoken, csrf_token)
 
     def log_message(format, *args):
         pass
 
 def shutdown_server():
-    print("Shutting down...")
     thread = Thread(target = http_server.shutdown)
     thread.start()
 
-def start_server():
+def start_server(port):
     global http_server
-    print(f"Starting server on port {port}...")
     http_server = HTTPServer(('127.0.0.1', port), OAuthRequestHandler)
     http_server.serve_forever()
 
-if __name__ == '__main__':
-    start_server()
+def retrieve_tokens(client_id, client_secret, redirect_url):
+    global auth_url, csrf_token, authcode
+    oauth = OAuth2(client_id=client_id, client_secret=client_secret)
+    auth_url, csrf_token = oauth.get_authorization_url(redirect_url)
+    port = urlparse(redirect_url).port
+    if not port:
+        print("The redirect URL must include an explicit port number.")
+        sys.exit(1)
+    import webbrowser
+    webbrowser.open(auth_url)
+    start_server(port)
+    access_token, refresh_token = oauth.authenticate(authcode)
+    return access_token, refresh_token
+
+def get_client(client_id, client_secret, access_token, refresh_token):
+    oauth = OAuth2(client_id=client_id, client_secret=client_secret,
+                   access_token=access_token, refresh_token=refresh_token)
+    client = Client(oauth)
+    return client
+
--- py/boxtools/cli.py
+++ py/boxtools/cli.py
@@ -1,11 +1,20 @@
 import os, os.path, sys
+import json
 import tomli
 
 default_config_toml = """\
 [auth]
 client-id = "(your client-id)"
 client-secret = "(your client-secret)"
-redirect-uri = "http://127.0.0.1:18444"
+redirect-url = "http://127.0.0.1:18444"
+"""
+
+usage = """\
+Usage: boxcli.sh command [args...]
+
+Commands:
+    auth
+    userinfo
 """
 
 config_dir = os.environ.get("BOXTOOLS_DIR", os.path.expanduser("~/.boxtools"))
@@ -28,9 +37,44 @@ with open(config_file, 'rb') as f:
     config = tomli.load(f)
     client_id = config['auth']['client-id']
     client_secret = config['auth']['client-secret']
-    redirect_uri = config['auth']['redirect-uri']
+    redirect_url = config['auth']['redirect-url']
 
 # Ensure the user actually modified the config file
 if client_id == "(your client-id)" or client_secret == "(your client-secret)":
     print(f"Edit '{config_file}' to supply a valid client ID and secret")
     sys.exit(0)
+
+args = sys.argv[1:]
+if len(args) == 0 or any(flag in args for flag in ('-h', '--help')):
+    print(usage, end="")
+    sys.exit(1)
+
+def save_tokens(access_token, refresh_token):
+    with open(tokens_file, 'wt') as f:
+        json.dump({ 'access_token' : access_token, 'refresh_token' : refresh_token }, f, 
+                  indent=2)
+
+def load_tokens_or_die():
+    if not os.path.exists(tokens_file):
+        print("You must first retrieve auth tokens by using the 'auth' command")
+        sys.exit(1)
+    with open(tokens_file, 'rt') as f:
+        tokendict = json.load(f)
+    return tokendict['access_token'], tokendict['refresh_token']
+
+command = args.pop(0)
+if command == "auth":
+    from .auth import retrieve_tokens
+    access_token, refresh_token = retrieve_tokens(client_id, client_secret, redirect_url)
+    save_tokens(access_token, refresh_token)
+    print(f"Tokens saved to {tokens_file}")
+else:
+    access_token, refresh_token = load_tokens_or_die()
+    from .auth import get_client
+    client = get_client(client_id, client_secret, access_token, refresh_token)
+    if command == "userinfo":
+        user = client.user().get()
+        print(f"User ID: {user.id}, Name: {user.name}")
+    else:
+        print(f"Unknown command '{command}'")
+        sys.exit(2)
